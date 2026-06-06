589c6ae3d16d920e1435d997a8556f012b4582a1
Sachin Karthikeya <114210321+SachinKarthikeya@users.noreply.github.com>
2026-06-06 10:28:04 +0530

    Upgraded LLM

--- llm_config.py
+++ llm_config.py
@@ -2,7 +2,7 @@ from crewai import LLM
 
 def llm_model():
     llm = LLM(
-        model="ollama/llama3.2:1b",
+        model="ollama/gemma3:4b",
         base_url="http://localhost:11434"
     )
     return llm 
